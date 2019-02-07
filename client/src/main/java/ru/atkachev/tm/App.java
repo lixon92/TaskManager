5d72532610adbd82cec26f016f6a2ea9f5669bd9
a.tkachev <lixon92@mail.ru>
2019-02-07 18:36:05 +0300

    the continue of the implementation of web services

---
+++ client/src/main/java/ru/atkachev/tm/App.java
@@ -0,0 +1,18 @@
+package ru.atkachev.tm;
+
+import ru.atkachev.tm.bootstrap.Bootstrap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        final Bootstrap bootstrap = new Bootstrap();
+        bootstrap.init();
+    }
+
+
+
+}
